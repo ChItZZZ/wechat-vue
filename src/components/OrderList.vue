704517cc607e6ae5991ab297be521e727722c5d4
fanliangandrew <986406153@qq.com>
2016-11-26 18:59:26 +0800

    order list

--- src/components/OrderList.vue
+++ src/components/OrderList.vue
@@ -1,95 +1,122 @@
 <template>
-  <div class="item-text">
-    <tr class="orderTitle">
-        <td style="width:15%">日期</td>
-        <td style="width:15%">商品</td>
-        <td style="width:15%">优惠</td>
-        <td style="width:15%">实付</td>
-        <td style="width:20%">支付方式</td>
-        <td style="width:10%">订单码</td>
-        <td style="width:10%">状态</td>
-    </tr>
-    <hr/>
-    <div id='ordersDiv'>
-      <tr v-for="order in orderslist">
-        <td style="width:15%">{{ order.date }</td>
-        <td style="width:15%">{{ order.name }}</td>
-        <td style="width:15%">{{ order.isCoupon }}</td>
-        <td style="width:15%">{{ order.payNum }}</td>
-        <td style="width:20%">{{ order.payWay }}</td>
-        <td style="width:10%">{{ order.orderID }}</td>
-        <td style="width:10%">{{ order.state }}</td> 
-      </tr>
+  <div class="order-container">
+    <div class="order-main order-one" style="border-top: 1px solid black">
+      <table>
+        <tr>
+          <td>日期</td>
+          <td>订单码</td>
+          <td>名称</td>
+          <td>数量</td>
+          <td>优惠</td>
+          <td>金额</td>
+          <td>状态</td>
+        </tr>
+        <hr/>
+      </table>
     </div>
-    <tr class="orderList">
-        <td style="width:15%">8/30</td>
-        <td style="width:15%">牛肉面</td>
-        <td style="width:15%">无</td>
-        <td style="width:15%">35</td>
-        <td style="width:20%">支付宝</td>
-        <td style="width:10%">0011</td>
-      
-      <td style="width:10%;">未付</td> 
-    </tr>
+
+    <div class="order-main order-two">
+      <div class="order-content " style="height: 100px">
+        <div class="order-cont">8/30</div>
+        <div class="order-cont">0001</div>
+        <div class="order-cont">红烧牛肉面</div>
+        <div class="order-cont">2</div>
+        <div class="order-cont">5</div>
+        <div class="order-cont">78</div>
+        <div class="order-unpaid">未付</div>
+      </div>
+      <div class="order-content " style="height: 100px">
+        <div class="order-cont">8/30</div>
+        <div class="order-cont">0001</div>
+        <div class="order-cont">红烧牛肉面</div>
+        <div class="order-cont">2</div>
+        <div class="order-cont">5</div>
+        <div class="order-cont">78</div>
+        <div class="order-paid">已付</div>
+      </div>
+    </div>
+    <div class="order-main order-two">
+      <div class="order-content " style="height: 100px">
+        <div class="order-cont">阿森纳地哦啊是你的</div>
+        <div class="order-cont">IQ网合肥南送</div>
+        <div class="order-cont">懊恼放弃我让你我</div>
+      </div>
+    </div>
+ 
   </div>
 </template>
-
-<script>
-  import { mapGetters, mapActions } from 'vuex'
-  export default {
-    computed: {
-      ...mapGetters({
-        curBarCount: 'navBarCount',
-      })
-    },
-    props: {
-      items: {
-        type: Array,
-        required: true
-      }
-    },
-    orderslist:function(){
-      var data = [];
-      data =[
-        {date:'8/30',name:'牛肉面',isCoupon:'无',payNum:'35',payWay:'AliPay',orderID:'0011',state:'未付'},
-        {date:'8/30',name:'牛肉面',isCoupon:'无',payNum:'35',payWay:'AliPay',orderID:'0011',state:'未付'},
-        {date:'8/30',name:'牛肉面',isCoupon:'无',payNum:'35',payWay:'AliPay',orderID:'0011',state:'未付'},
-        {date:'8/30',name:'牛肉面',isCoupon:'无',payNum:'35',payWay:'AliPay',orderID:'0011',state:'未付'}
-        ];
-      return data;
-    }
-
-
-    }
-
-
-</script>
-
 <style scoped>
-  .item-text {
-    flex: 7;
-    text-align: left;
+  .order-container {
+    text-align: center;
+    flex-grow: 5;
+    overflow-y: auto;
+    overflow-x: hidden;
+    height: 100%;
+    min-height: 100vh;
   }
 
-  .item-text tr {
-    margin: 5px;
+  .order-title {
+    border-bottom: 1px solid white;
+    background-color: darkred;
+    color: white;
+    font-size: 12px;
+    height: 33px;
+    line-height: 33px;
+  }
+  .order-main{
+    min-height: 130px;
   }
 
-  .item-text tr td{
-      text-align: center;      
+  .order-one{
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
   }
 
-  .orderTitle {
-    margin-top: 20px;
-    height: 20px;
-    line-height: 46px;
-    font-size: 18px;
+  .order-cont{
+    flex: 1;
+    color: black;
+    font-size: 10px;
+    position: relative;
+    bottom: 0;
+    align-items: stretch;
+    flex-direction: column;
+    align-self: stretch;
+    align-self: -webkit-stretch;
+    border-right: 0px solid black;
   }
-  .orderList {
-    height: 20px;
-    line-height: 46px;
-    font-size: 18px;
+  .order-unpaid{
+    flex: 1;
+    color: black;
+    font-size: 10px;
+    position: relative;
+    bottom: 0;
+    align-items: stretch;
+    flex-direction: column;
+    align-self: stretch;
+    align-self: -webkit-stretch;
+    background-image: url();
+  }  
+  .order-paid{
+    flex: 1;
+    color: black;
+    font-size: 10px;
+    position: relative;
+    bottom: 0;
+    align-items: stretch;
+    flex-direction: column;
+    align-self: stretch;
+    align-self: -webkit-stretch;
+    border-right: 0px solid black;
   }
-
+  .order-content{
+    display: flex;
+    display: -webkit-flex;
+  }
+  table{
+    width: 100%;
+    font-size: 10px;
+    margin-top: 10px;
+  }
 </style>
-
